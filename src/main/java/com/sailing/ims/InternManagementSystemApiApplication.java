3c3b4eefc6ef3fec91a7afe21c3b221aacf5cfe8
Hiep <doanmanhhiep.2003@gmail.com>
2024-06-23 00:33:22 +0700

    first commit

---
+++ src/main/java/com/sailing/ims/InternManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.sailing.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternManagementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternManagementSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sailing/ims/InternManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sailing.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternManagementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
